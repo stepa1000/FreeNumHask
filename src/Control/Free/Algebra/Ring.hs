8ea1819b708490ba9706c895207b8554da6e9288
Anisimkov Stepan <stepa100023@gmail.com>
2021-12-02 11:29:40 +0600

    add free ring

--- src/Control/Free/Algebra/Ring.hs
+++ src/Control/Free/Algebra/Ring.hs
@@ -58,3 +58,39 @@ proMapFixDistributive :: Distributive b
                        => p a b
                        -> AltArr p (FixDistributive a) b
 proMapFixDistributive pab = proLetFoldFix pab (joinAltArr $ proMapFreeDistributive id)
+
+data FreeRing a =
+    FRM (FreeMultiplicative a)
+  | FRS (FreeSubtractive a)
+  deriving (Traversable, Foldable, Functor)
+
+annihilationFreeRing :: FreeRing a -> Either (FreeSubtractive a) (FreeMultiplicative a)
+annihilationFreeRing (FRS fa) = Left fa
+annihilationFreeRing (FRM fm) = Right fm
+
+type FixRing a = FixLetVar a FreeRing
+
+instance Additive (FixRing b) where
+  x + y = Fix $ LetExp $ Exp $ FRS $ FAdditive $ FAdd x y
+  zero = Fix $ LetExp $ Exp $ FRS $ FAdditive Zero
+
+instance Multiplicative (FixRing b) where
+  x * y = Fix $ LetExp $ Exp $ FRM $ Mult x y
+  one = Fix $ LetExp $ Exp $ FRM One
+
+instance Subtractive (FixRing a) where
+  negate = Fix . LetExp . Exp . FRS . FNegate
+  x - y = Fix $ LetExp $ Exp $ FRS $ FSubtract x y
+
+proMapFreeRing :: (Ring b, Distributive b, Subtractive b)
+               => p a b
+               -> AltArr p (FreeRing a) b
+proMapFreeRing pab = proc fadd -> do
+  b <- id ||| id <<< proMapFreeSubtractive' pab +++ proMapFreeMultiplicative pab
+    -< annihilationFreeRing fadd
+  returnA -< b
+
+proMapFixRing :: (Ring b, Distributive b, Subtractive b)
+              => p a b
+              -> AltArr p (FixRing a) b
+proMapFixRing pab = proLetFoldFix pab (joinAltArr $ proMapFreeRing id)
