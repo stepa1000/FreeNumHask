b6ee09322b7af54ba1950c5215fde19d483ee1ff
Anisimkov Stepan <stepa100023@gmail.com>
2021-12-02 15:59:33 +0600

    add postlude

---
+++ src/Control/Free/Algebra/Postlude.hs
@@ -0,0 +1,10 @@
+module Control.Free.Algebra.Postlude (module X) where
+
+import Control.Free.Algebra.Cse as X
+
+import Control.Free.Algebra.Multiplicative as X
+import Control.Free.Algebra.Additive as X
+import Control.Free.Algebra.Ring as X
+import Control.Free.Algebra.Module as X
+
+import Control.Free.Algebra.Arrow as X
